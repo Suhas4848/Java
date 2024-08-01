63b9596f24c79207be1d430cf6ac0a3db1db26bd
suhas thoke <159158216+Suhas4848@users.noreply.github.com>
2024-08-01 21:03:10 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class MinMax{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+
+        System.out.println("Enter the array elements ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println(arr);
+        int max = arr[0];
+        int min=arr[0];
+      
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Max no is " + max);
+        System.out.println("Min no is " + min);
+
+    }
+}
